526573d14177ec88842800a0ff44595c0a85581b
pablolancini <lancinipablo@gmail.com>
2023-12-13 16:50:04 +0100

    Modifica Script registrazione.py

--- registrazione.py
+++ registrazione.py
@@ -7,12 +7,12 @@ redis_password = "Password"
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
 
 def controllare_disponibilita_nome_utente(nome_utente):
-    return not redis_client.hexists("prova", nome_utente)
+    return not redis_client.hexists("utenti", nome_utente)
 
 def registrare_utente(nome_utente, password,dnd):
     if not controllare_disponibilita_nome_utente(nome_utente):
         return f"Il nome utente '{nome_utente}' è già in uso. Scegli un altro nome."
-    redis_client.hset("prova", nome_utente,password)
+    redis_client.hset("utenti", nome_utente,password)
     redis_client.hset("stato", nome_utente, dnd)
     return f"Registrazione avvenuta con successo per {nome_utente}"
 
