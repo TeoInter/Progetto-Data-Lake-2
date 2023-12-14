1a7ff04029c9a40f1cdceee6716cab7695eaa43f
pablolancini <lancinipablo@gmail.com>
2023-12-14 12:45:41 +0100

    Modifica Script Main

--- Main.py
+++ Main.py
@@ -13,12 +13,18 @@ if scelta == '1':
 elif scelta == '2':
     nome_utente = Funzioni.login()
     if nome_utente is not None:
-        print("Accesso eseguito, cosa vuoi fare?\n1)Attiva/disattiva Dnd\n2)Visualizza Lista Amici")
+        print("Accesso eseguito, cosa vuoi fare?\n1)Attiva/disattiva Dnd\n2)Visualizza Lista Amici\n3)Ricerca Utente")
         selezione = input("Fai la tua scelta: ")
         if int(selezione) == 1:
             Funzioni.visualizza_stato(nome_utente)
         if int(selezione) == 2:
-            print(Funzioni.ottenere_lista_amici(nome_utente))
+            lista_amici = Funzioni.ottenere_lista_amici(nome_utente)
+            print(lista_amici)
+        if int(selezione) == 3:
+            utente_cercato = Funzioni.cercare_utenti()
+            print(f"Desideri aggiungere {utente_cercato} agli amici (y/n)? ")
+            if input() == 'y':
+                Funzioni.aggiungere_amico(nome_utente=nome_utente, nome_amico=utente_cercato)
         else:
             print("Errore, selezione non valida")
 elif scelta == '3':
